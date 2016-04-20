e7fff0fabe00ecfe861d8b960a174ebbf7a5efec
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-20 12:35:09 -0400

    add uniquness validation to company name and URL

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ActiveRecord::Base
-  validates :name, :headquarters_location, :url, :stack, :industry, presence: true
-  validates :open_source_projects_url, allow_blank: true
+  validates :name, :url, :headquarters_location, :stack, :industry, presence: true
+  validates :name, :url, uniqueness: true
   validates :about_us, length: { in: 1..500 }
   validates :size, numericality: { greater_than_or_equal_to: 1 }
   validates :founded_in, numericality: { less_than_or_equal_to: $CURRENT_YEAR }, length: { is: 4 }
