d2fef6cca055206a24f6c06b315dd2c66c07dafc
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-20 11:48:55 -0400

    add reasonable validations for companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ActiveRecord::Base
+  # work on possible validation of :open_source_projects_url,
+  validates :name, :headquarters_location, :url, :stack, :industry, presence: true
+  validates :about_us, length: { in: 1..500 }
+  validates :size, :founded_in, numericality: { only_integer: true }
 end
