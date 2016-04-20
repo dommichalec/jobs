600feea2a79f087001c950fb1d465ae48faa3f1e
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-20 11:54:15 -0400

    decouple numericality and length for founded_in from company size

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,6 @@ class Company < ActiveRecord::Base
   # work on possible validation of :open_source_projects_url,
   validates :name, :headquarters_location, :url, :stack, :industry, presence: true
   validates :about_us, length: { in: 1..500 }
-  validates :size, :founded_in, numericality: { only_integer: true }
+  validates :size, numericality: { only_integer: true }
+  validates :founded_in, numericality: { only_integer: true }, length: { is: 4 }
 end
