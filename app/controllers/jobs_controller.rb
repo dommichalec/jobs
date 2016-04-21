0158c476c5ebed108b3368779e833c6334056206
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-21 14:04:16 -0400

    add edit and update actions for job postings

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -18,12 +18,26 @@ class JobsController < ApplicationController
     @company = Company.find(params[:company_id])
     @job = @company.jobs.create(job_params)
     if @job.save
-      redirect_to company_path(@company), notice: "The #{@job.title} position has been successfully added to #{@company.name}'s profile!'!"
+      redirect_to company_path(@company), notice: "The #{@job.title} position has been successfully added to #{@company.name}'s profile!"
     else
       render :new
     end
   end
 
+  def edit
+    @company = Company.find(params[:company_id])
+    @job = @company.jobs.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:company_id])
+    @job = @company.jobs.find(params[:id])
+    if @job.save
+      redirect_to company_path(@company), notice: "The #{@job.title} posting has been successfully updated on #{@company.name}'s profile!"
+    else
+      render :edit
+    end
+  end
   private
 
   def job_params
