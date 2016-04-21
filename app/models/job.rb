4eec90ccffb6c6a0a87bb70af7a2398cbeeed143
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-04-21 15:55:02 -0400

    add validations to jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,11 @@
 class Job < ActiveRecord::Base
-  belongs_to :company
+validates :title, :how_apply, presence: true
+validates :description, length: { in: 1..500 }
+validates :time_spent_in_office, numericality: { greater_than_or_equal_to: 0 }
+validates :available_positions, numericality: { greater_than_or_equal_to: 1 }
+
+belongs_to :company
+
   def filled?
     available == false
   end
